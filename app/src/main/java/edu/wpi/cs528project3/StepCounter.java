115dba5e5d5b14e223fe9ed7c0d7c50a7326f2f1
kevin <kevin.r.fortier@gmail.com>
2020-10-15 15:40:20 -0400

    StepCounter start

---
+++ app/src/main/java/edu/wpi/cs528project3/StepCounter.java
@@ -0,0 +1,46 @@
+package edu.wpi.cs528project3;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class StepCounter implements SensorEventListener {
+    private static int SMOOTHING_WINDOW_SIZE = 20;
+
+    public static SensorManager mSensorManager;
+    public static Sensor mSensorCount, mSensorAcc;
+    private float mRawAccelValues[] = new float[3];
+
+    // smoothing accelerometer signal variables
+    private float mAccelValueHistory[][] = new float[3][SMOOTHING_WINDOW_SIZE];
+    private float mRunningAccelTotal[] = new float[3];
+    private float mCurAccelAvg[] = new float[3];
+    private int mCurReadIndex = 0;
+
+    public static float mStepCounter = 0;
+    public static float mStepCounterAndroid = 0;
+    public static float mInitialStepCount = 0;
+
+    private double mGraph1LastXValue = 0d;
+    private double mGraph2LastXValue = 0d;
+
+    private double lastMag = 0d;
+    private double avgMag = 0d;
+    private double netMag = 0d;
+
+    //peak detection variables
+    private double lastXPoint = 1d;
+    double stepThreshold = 1.0d;
+    double noiseThreshold = 2d;
+    private int windowSize = 10;
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
